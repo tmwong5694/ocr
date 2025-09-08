0e4e7f56dc16eb775b11ce40172ba63c941b3161
Tim <tmwong031@gmail.com>
2025-09-08 23:10:05 +0800

    rearrange functions

--- src/DeepLearning/mnist.py
+++ src/DeepLearning/mnist.py
@@ -12,6 +12,42 @@ import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "mps")
 
+def get_batch_accuracy(output, y, N):
+    pred = output.argmax(dim=1, keepdim=True)
+    correct = pred.eq(y.view_as(pred)).sum().item()
+    return correct / N
+
+def train():
+    loss = 0
+    accuracy = 0
+
+    model.train()
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
+        output = model(x)
+        optimizer.zero_grad()
+        batch_loss = loss_function(output, y)
+        batch_loss.backward()
+        optimizer.step()
+
+        loss += batch_loss.item()
+        accuracy += get_batch_accuracy(output, y, train_N)
+    print("Train - Loss: {:.4f} Accuracy: {:.4f}".format(loss, accuracy))
+
+def validate():
+    loss = 0
+    accuracy = 0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in valid_loader:
+            x, y = x.to(device), y.to(device)
+            output = model(x)
+
+            loss += loss_function(output, y).item()
+            accuracy += get_batch_accuracy(output, y, valid_N)
+    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
 trans = v2.Compose([
     # v2.ToTensor() is deprecated
     v2.ToImage(),
@@ -79,42 +115,6 @@ optimizer = Adam(model.parameters())
 
 train_N = len(train_loader.dataset)
 valid_N = len(valid_loader.dataset)
-
-def get_batch_accuracy(output, y, N):
-    pred = output.argmax(dim=1, keepdim=True)
-    correct = pred.eq(y.view_as(pred)).sum().item()
-    return correct / N
-
-def train():
-    loss = 0
-    accuracy = 0
-
-    model.train()
-    for x, y in train_loader:
-        x, y = x.to(device), y.to(device)
-        output = model(x)
-        optimizer.zero_grad()
-        batch_loss = loss_function(output, y)
-        batch_loss.backward()
-        optimizer.step()
-
-        loss += batch_loss.item()
-        accuracy += get_batch_accuracy(output, y, train_N)
-    print("Train - Loss: {:.4f} Accuracy: {:.4f}".format(loss, accuracy))
-
-def validate():
-    loss = 0
-    accuracy = 0
-
-    model.eval()
-    with torch.no_grad():
-        for x, y in valid_loader:
-            x, y = x.to(device), y.to(device)
-            output = model(x)
-
-            loss += loss_function(output, y).item()
-            accuracy += get_batch_accuracy(output, y, valid_N)
-    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
 
 
 import torch._dynamo
