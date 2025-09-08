acc6ec2c37a3a40fcec635634e8087938f9bf428
Tim <tmwong031@gmail.com>
2025-09-09 00:56:54 +0800

    defined training function

--- src/DeepLearning/tl.py
+++ src/DeepLearning/tl.py
@@ -3,10 +3,18 @@ import torchvision.models as models
 from torchvision.transforms import v2
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
+import torch.optim as optim
 import torch.nn as nn
+
+import torch._dynamo
+
+# Set "mps" as the accelerator
+device = torch.device("mps")
 
 weights = models.ResNet18_Weights.DEFAULT
 model = models.resnet18(weights=weights)
+# Suppress errors during compilation
+torch._dynamo.config.suppress_errors = True
 
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 10)
@@ -26,8 +34,31 @@ test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform,
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+
+def train_model(model, train_loader, criterion, optimizer, epochs=5):
+    model.train()
+    for epoch in range(epochs):
+        running_loss = 0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            # Predict output class using input
+            outputs = model(inputs)
+            # Calculate the loss
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+    return model
+
 if __name__ == "__main__":
 
+    model.to(device)
+    # model = torch.compile(model)
+    train_model(model=model, train_loader=train_loader, criterion=criterion, optimizer=optimizer)
 
 
 
