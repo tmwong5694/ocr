f9478043c427eb4e59ea8f41cf6df8b3e7d15a9c
tmwong5694 <w.tinyan99@gmail.com>
2026-04-03 00:59:21 +0800

    utils of logger

---
+++ src/utils/set_logger.py
@@ -0,0 +1,31 @@
+import logging
+from pathlib import Path
+
+def set_logger(name: str, level: int, logger_path: str | Path) -> logging.Logger:
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console_handler = logging.StreamHandler()
+    logger_path = Path(logger_path)
+    if not logger_path.parent.exists():
+        logger_path.parent.mkdir(exist_ok=True, parents=True)
+    file_handler = logging.FileHandler(logger_path, encoding="utf-8")
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    console_handler.setFormatter(formatter)
+
+    return logger
+
+if __name__ == "__main__":
+
+    logg = set_logger(__name__, logging.INFO, logger_path=Path("logs") / "testing.log")
+    logg.INFO("Set up success!")
+    pass
