54beaf1e1892e1babf81e91cbed66d239b8f522b
tmwong5694 <w.tinyan99@gmail.com>
2026-04-03 01:18:06 +0800

    rotate log file daily

--- src/utils/set_logger.py
+++ src/utils/set_logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 def set_logger(name: str, level: str, logger_path: str | Path) -> logging.Logger:
@@ -16,15 +17,17 @@ def set_logger(name: str, level: str, logger_path: str | Path) -> logging.Logger
     logger.setLevel(logging_levels[level])
 
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s',
+        '%(asctime)s - %(name)s - %(levelname)-8s - %(filename)s - %(funcName)s():%(lineno)d - %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     console_handler = logging.StreamHandler()
+
     logger_path = Path(logger_path)
     if not logger_path.parent.exists():
         logger_path.parent.mkdir(exist_ok=True, parents=True)
-    file_handler = logging.FileHandler(logger_path, encoding="utf-8")
+    file_handler = TimedRotatingFileHandler(logger_path, encoding="utf-8", when="midnight", interval=1)
+    file_handler.suffix = "%Y-%m-%d"
 
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
@@ -36,5 +39,5 @@ def set_logger(name: str, level: str, logger_path: str | Path) -> logging.Logger
 if __name__ == "__main__":
 
     logg = set_logger(__name__, "info", logger_path=Path("logs") / "testing.log")
-    logg.debug("Set up success!")
+    logg.info("Set up success!")
     pass
