6e0c1fcfec2968207bb6fa856ee289f8ebcc70e9
tmwong5694 <w.tinyan99@gmail.com>
2026-04-03 16:04:33 +0800

    use tqdm logging

--- src/utils/set_logger.py
+++ src/utils/set_logger.py
@@ -1,10 +1,31 @@
 import logging
+import sys
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
+from tqdm import tqdm
 
-def set_logger(name: str, level: str, logger_path: str | Path) -> logging.Logger:
+
+class TqdmLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.write(msg, file=sys.stderr)
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception:
+            self.handleError(record)
+
+
+def set_logger(name: str, level: str, logger_path: str | Path, use_tqdm_handler: bool = False) -> logging.Logger:
 
     logger = logging.getLogger(name)
+
+    if logger.hasHandlers():
+        logger.handlers.clear()
 
     logging_levels = {
         "debug": logging.DEBUG,
@@ -21,20 +42,22 @@ def set_logger(name: str, level: str, logger_path: str | Path) -> logging.Logger
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    if use_tqdm_handler:
+        console_handler = TqdmLoggingHandler()
+    else:
+        console_handler = logging.StreamHandler()
 
+    console_handler.setLevel(logging_levels[level])
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
     logger_path = Path(logger_path)
     if not logger_path.parent.exists():
         logger_path.parent.mkdir(exist_ok=True, parents=True)
     file_handler = TimedRotatingFileHandler(logger_path, encoding="utf-8", when="midnight", interval=1)
     file_handler.suffix = "%Y-%m-%d"
-
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-    console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)    
 
     return logger
 
