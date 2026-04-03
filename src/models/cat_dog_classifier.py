fd839bdc0556633a4f44c9650091e23361a6af22
tmwong5694 <w.tinyan99@gmail.com>
2026-04-04 01:22:08 +0800

    self-build model

---
+++ src/models/cat_dog_classifier.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class CatDogClassifier(nn.Module):
+
+    def __init__(self, num_classes=10, **kwargs):
+        super().__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32 * 56 * 56, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+
+    def forward(self, x):
+        # Pass input through the feature extractor
+        x = self.features(x)
+        # Pass flattened output to the classifier
+        x = self.classifier(x)
+        return x
