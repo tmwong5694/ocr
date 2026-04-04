79327e7451e6fd5081dcfa447c605b09e7fbf545
tmwong5694 <w.tinyan99@gmail.com>
2026-04-04 12:34:56 +0800

    restructure the paths

--- train.py
+++ train.py
@@ -15,9 +15,6 @@ from loguru import logger
 from src.utils.set_logger import set_loguru
 
 
-log_path = Path("logs") / "resnet_training.log"
-set_loguru(level="info", logger_path=log_path)
-
 
 def get_model(cfg: dict):
     model_name = cfg['model']['name']
@@ -45,6 +42,13 @@ def main(config_path: Path | str) -> None:
         raise FileNotFoundError(f"config file '{config_path}' does not exist")
     cfg = load_config(config_path)
 
+    log_dir = Path(cfg['paths']['experiments_root']) / cfg['experiment_name'] / cfg['run_name'] / cfg['paths'].get(
+        'logs_dirname', 'logs')
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_path = log_dir / f"training_{cfg['model']['name']}.log"
+
+    set_loguru(level="info", logger_path=log_path)
+
     if torch.backends.mps.is_available():
         DEVICE = torch.device("mps")
     elif torch.cuda.is_available():
@@ -57,10 +61,10 @@ def main(config_path: Path | str) -> None:
     # 2. Data Pipeline
     logger.info("Loading datasets...")
     train_loader, val_loader, test_loader = get_dataloaders(
-        data_dir=cfg['data_dir'],
-        batch_size=cfg['batch_size'],
+        data_dir=cfg['data']['dataset_dir'],
+        batch_size=cfg['data']['batch_size'],
         seed=cfg['seed'],
-        num_workers=cfg['num_workers']
+        num_workers=cfg['data']['num_workers']
     )
     logger.info("Train: {} | Val: {} | Test: {}", len(train_loader), len(val_loader), len(test_loader))
 
@@ -71,10 +75,17 @@ def main(config_path: Path | str) -> None:
     # 4. Training Components
     criterion = nn.CrossEntropyLoss()
     trainable_params = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = optim.Adam(trainable_params, lr=cfg['learning_rate'])
+    optimizer = optim.Adam(trainable_params, lr=cfg['training']['optimizer']['learning_rate'])
 
     # 5. Execute Training Engine
-    save_dir = Path(cfg['save_path']).parent
+    save_dir = (
+        Path(cfg['paths']['experiments_root']) /
+        cfg['experiment_name'] /
+        cfg['run_name'] /
+        cfg['paths']['checkpoints_dirname']
+    )
+    save_path = save_dir / "best_model.pth"
+
     save_dir.mkdir(parents=True, exist_ok=True)
     logger.info("Starting training engine...")
     trained_model, history = train_model(
@@ -83,15 +94,17 @@ def main(config_path: Path | str) -> None:
         val_loader=val_loader,
         criterion=criterion,
         optimizer=optimizer,
-        num_epochs=cfg['num_epochs'],
+        num_epochs=cfg['training']['num_epochs'],
         device=DEVICE,
-        save_path=cfg['save_path']
+        save_path=save_path
     )
 
-    image_path = Path("outputs") / "plots" / f"loss_resnet18_epoch{cfg['num_epochs']}_{date.today():%Y.%m.%d}.jpeg"
-    plot_loss(train_loss=history['train_loss'], val_loss=history["val_loss"], save_path=image_path)
+    image_dir = Path("outputs") / "plots"
+    image_name = f"loss_{cfg['model']['name']}_epoch{cfg['training']['num_epochs']}_{date.today():%Y.%m.%d}.jpeg"
 
-    logger.info("\nPipeline complete! Weights saved in '{}'.", cfg['save_path'])
+    plot_loss(train_loss=history['train_loss'], val_loss=history["val_loss"], save_path=image_dir / image_name)
+
+    logger.info("\nPipeline complete! Weights saved in '{}'.", save_path)
 
 
 if __name__ == "__main__":
