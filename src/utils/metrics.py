c40020825ccb9cc9eec9819b7045f9fa7c23e019
tmwong5694 <w.tinyan99@gmail.com>
2026-04-05 01:42:21 +0800

    add docstring, renamed variables

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,5 +1,18 @@
-def get_batch_accuracy(output, y, N):
+from torch import Tensor
 
-    pred = output.argmax(dim=1)
-    correct = (pred == y.view_as(pred)).sum().item()
-    return correct / N
+def get_batch_accuracy(predictions: Tensor, ground_truths: Tensor, batch_size: int):
+    """
+    Takes predictions and ground truths and computes the accuracy of the predictions in this batch.
+
+    Args:
+        predictions (Tensor): Predictions output from the model
+        ground_truths (Tensor): Ground truth output from the dataloader
+        batch_size (int): size of the current batch
+
+    Returns:
+        float: accuracy of the predictions
+    """
+    pred = predictions.argmax(dim=1)
+    # Ensure same dimensions of pred and ground_truths
+    correct = (pred == ground_truths.view_as(pred)).sum().item()
+    return correct / batch_size
