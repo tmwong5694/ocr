309b95f90d72a644b80faea16f2e96f0339ae836
tmwong5694 <w.tinyan99@gmail.com>
2026-04-04 18:18:17 +0800

    remove underscores

--- src/models/factory.py
+++ src/models/factory.py
@@ -2,11 +2,11 @@ from src.models.cat_dog_classifier import CatDogClassifier
 from src.models.transfer_resnet import TransferResNet
 
 def get_model(model_name: str, model_params: dict):
-    model_name_lower = model_name.lower()
+    model_name_clean = model_name.lower().replace("_", "")
 
-    if model_name_lower == "transferresnet":
+    if model_name_clean == "transferresnet":
         return TransferResNet(**model_params)
-    elif model_name_lower == "catdogclassifier":
+    elif model_name_clean == "catdogclassifier":
         return CatDogClassifier(**model_params)
     else:
         raise ValueError(f"Model {model_name} is not supported!")
