5420085e370cbb0d4634ecf16b8d1440032088ad
tmwong5694 <w.tinyan99@gmail.com>
2026-04-04 22:35:15 +0800

    Random resized crop and erase

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,21 +1,25 @@
 import torch
 from torchvision.transforms import v2
 
-def get_transforms(img_size: tuple = (224, 224)):
+def get_transforms(img_size: int = 224):
 
     normalize = v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     train_transform = v2.Compose([
-        v2.Resize(img_size),
+        v2.RandomResizedCrop(size=(img_size, img_size), scale=(0.8, 1.0), antialias=True), # Remove jagged lines
         v2.RandomHorizontalFlip(p=0.5),
         v2.RandomRotation(degrees=20),
         v2.RandomApply([v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.5),
         v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True),
-        normalize
+        normalize,
+        v2.RandomErasing(p=0.2, scale=(0.02, 0.15)) # Must follow ToDtype and normalization
     ])
 
     eval_transform = v2.Compose([
-        v2.Resize(img_size),
+        # Resize the shortest edge to slightly larger than the target (standard is 256 for 224)
+        v2.Resize(int(img_size / 0.875), antialias=True),
+        # Crop the exact center to ensure perfectly square, uniform batch sizes
+        v2.CenterCrop(img_size),
         v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True),
         normalize
