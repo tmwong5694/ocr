9b50fef6857dd53df70f2c4ef81f427b54b14669
tmwong5694 <w.tinyan99@gmail.com>
2026-04-06 17:01:44 +0800

    plot scaled confusion matrix

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def plot_loss(
         train_loss: list,
@@ -30,4 +32,42 @@ def plot_loss(
         # plt.show() clears the canvas, need to put at the end
         plt.show()
     # Close the active canvas
+    plt.close()
+
+def plot_confusion_matrix(
+        y_true: list,
+        y_pred: list,
+        class_names: list[str],
+        figsize: tuple = (8, 6),
+        title: str = "Confusion Matrix",
+        xlabel: str = "True Label",
+        ylabel: str = "Predicted Label",
+        save_path: str | Path | None = None,
+        show: bool = False
+) -> None:
+    cm = confusion_matrix(y_true=y_true, y_pred=y_pred, normalize="true")
+    plt.figure(figsize=figsize)
+    sns.heatmap(
+        cm,
+        annot=True,
+        fmt='d',
+        cmap='Blues',
+        xticklabels=class_names,
+        yticklabels=class_names
+    )
+
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.tight_layout()
+
+    if save_path:
+        save_path = Path(save_path)
+        if not save_path.parent.exists():
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+        plt.savefig(save_path, dpi=300)
+
+    if show:
+        plt.show()
+
     plt.close()
