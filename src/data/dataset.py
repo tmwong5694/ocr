7e2944baa57c434b0d5e697bad8b9301ca273799
tmwong5694 <w.tinyan99@gmail.com>
2026-04-09 00:04:16 +0800

    dataloaders with multiple fns

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -5,6 +5,62 @@ from torch.utils.data import DataLoader, random_split, Subset, Dataset
 from torchvision import datasets
 from src.data.transforms import get_transforms
 
+def _get_imagefolder_datasets(
+        data_dir: str | Path,
+        train_transform,
+        eval_transform,
+        split_ratio: list[int | float] | tuple[int | float, ...],
+        seed: int = 42
+) -> tuple[Dataset, Dataset, Dataset]:
+
+    # Instantiate all at once with respective transformation method
+    train_data = datasets.ImageFolder(root=data_dir, transform=train_transform)
+    val_data = datasets.ImageFolder(root=data_dir, transform=eval_transform)
+    test_data = datasets.ImageFolder(root=data_dir, transform=eval_transform)
+
+    # Use indices to select respective train, val or test data
+    total_indices = range(len(train_data))
+    generator = torch.Generator().manual_seed(seed)
+    train_indices, val_indices, test_indices = random_split(
+        dataset=total_indices,
+        lengths=split_ratio,
+        generator=generator
+    )
+
+    train_dataset = Subset(train_data, indices=train_indices)
+    val_dataset = Subset(val_data, indices=val_indices)
+    test_dataset = Subset(test_data, indices=test_indices)
+
+    return train_dataset, val_dataset, test_dataset
+
+def _get_mnist_datasets(
+        data_dir: str | Path,
+        train_transform,
+        eval_transform,
+        split_ratio: list[int | float] | tuple[int | float, ...],
+        seed: int = 42
+) -> tuple[Dataset, Dataset, Dataset]:
+    
+    to_download = True
+    train_dataset_full = datasets.MNIST(data_dir, train=True, download=to_download, transform=train_transform)
+    val_dataset_full = datasets.MNIST(data_dir, train=True, download=to_download, transform=eval_transform)
+
+    split_indices = range(len(train_dataset_full))
+    generator = torch.Generator().manual_seed(seed)
+    train_indices, val_indices = random_split(
+        dataset=split_indices,
+        lengths=split_ratio,
+        generator=generator
+    )
+
+    train_data = Subset(train_dataset_full, train_indices)
+    val_data = Subset(val_dataset_full, val_indices)
+    train_dataset = Subset(train_data, indices=train_indices)
+    val_dataset = Subset(val_data, indices=val_indices)
+
+    test_dataset = datasets.MNIST(data_dir, train=False, download=to_download, transform=eval_transform)
+    
+    return train_dataset, val_dataset, test_dataset
 
 def _get_loaders(
         train_ds: Dataset,
@@ -47,77 +103,42 @@ def _get_loaders(
     )
 
 def get_dataloaders(
-        data_dir: str| Path = "./data",
-        batch_size: int = 64,
-        split_ratios: list[int | float] | tuple[int | float, ...] = (0.8, 0.1, 0.1),
-        seed: int = 42
-) -> tuple[DataLoader, DataLoader, DataLoader]:
-    transforms_dict = get_transforms()
-    train_transform = transforms_dict['train']
-    eval_transform = transforms_dict['eval']
-
-    # Instantiate all at once with respective transformation method
-    train_data = datasets.ImageFolder(root=data_dir, transform=train_transform)
-    val_data = datasets.ImageFolder(root=data_dir, transform=eval_transform)
-    test_data = datasets.ImageFolder(root=data_dir, transform=eval_transform)
-
-    # Use indices to select respective train, val or test data
-    total_indices = range(len(train_data))
-    generator = torch.Generator().manual_seed(seed)
-    train_indices, val_indices, test_indices = random_split(
-        dataset=total_indices,
-        lengths=split_ratios,
-        generator=generator
-    )
-
-    train_dataset = Subset(train_data, indices=train_indices)
-    val_dataset = Subset(val_data, indices=val_indices)
-    test_dataset = Subset(test_data, indices=test_indices)
-
-    train_loader, val_loader, test_loader = _get_loaders(
-        train_dataset,
-        val_dataset,
-        test_dataset,
-        batch_size=batch_size
-    )
-
-    return train_loader, val_loader, test_loader
-
-def get_MNIST_loaders(
+        dataset_name: str,
         data_dir: str | Path = "./data",
-        download: bool = True,
+        split_ratio: list[int | float] | tuple[int | float, ...] = None,
         batch_size: int = 64,
-        split_ratios: list[int | float] | tuple[int | float, ...] = (50000, 10000),
         seed: int = 42
 ) -> tuple[DataLoader, DataLoader, DataLoader]:
+
+    if split_ratio is None:
+        raise ValueError("Split ratio cannot be None")
 
     transforms_dict = get_transforms()
     train_transform = transforms_dict['train']
     eval_transform = transforms_dict['eval']
 
-    train_dataset_full = datasets.MNIST(data_dir, train=True, download=download, transform=train_transform)
-    val_dataset_full = datasets.MNIST(data_dir, train=True, download=download, transform=eval_transform)
-
-    split_indices = range(len(train_dataset_full))
-    generator = torch.Generator().manual_seed(seed)
-    train_indices, val_indices = random_split(
-        dataset=split_indices,
-        lengths=split_ratios,
-        generator=generator
-    )
-
-    train_data = Subset(train_dataset_full, train_indices)
-    val_data = Subset(val_dataset_full, val_indices)
-    train_dataset = Subset(train_data, indices=train_indices)
-    val_dataset = Subset(val_data, indices=val_indices)
-
-    test_dataset = datasets.MNIST(data_dir, train=False, download=download, transform=eval_transform)
+    if dataset_name.lower() in ("imagefolder", "image_folder"):
+        train_dataset, val_dataset, test_dataset = _get_imagefolder_datasets(
+            data_dir=data_dir,
+            train_transform=train_transform,
+            eval_transform=eval_transform,
+            split_ratio=split_ratio,
+            seed=seed
+        )
+    elif dataset_name.lower() == "mnist":
+        train_dataset, val_dataset, test_dataset = _get_mnist_datasets(
+            data_dir=data_dir,
+            train_transform=train_transform,
+            eval_transform=eval_transform,
+            split_ratio=split_ratio,
+            seed=seed
+        )
 
     train_loader, val_loader, test_loader = _get_loaders(
         train_dataset,
         val_dataset,
         test_dataset,
-        batch_size=batch_size
+        batch_size=batch_size,
     )
 
     return train_loader, val_loader, test_loader
