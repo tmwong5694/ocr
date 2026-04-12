d3bf812cd89cd0b48be4e7b3cadcd442e1c45b65
tmwong5694 <w.tinyan99@gmail.com>
2026-04-13 01:54:22 +0800

    dispatch function for models

--- ml/src/ml_pipeline/models/factory.py
+++ ml/src/ml_pipeline/models/factory.py
@@ -1,12 +1,21 @@
 from ml_pipeline.models.cat_dog_classifier import CatDogClassifier
 from ml_pipeline.models.transfer_resnet import TransferResNet
+from ml_pipeline.models.mnist_classifier import MNISTClassifier
+
+
+MODEL_DISPATCH = {
+    "transferresnet": TransferResNet,
+    "catdogclassifier": CatDogClassifier,
+    "mnistclassifier": MNISTClassifier,
+}
+
 
 def get_model(model_name: str, model_params: dict):
     model_name_clean = model_name.lower().replace("_", "")
 
-    if model_name_clean == "transferresnet":
-        return TransferResNet(**model_params)
-    elif model_name_clean == "catdogclassifier":
-        return CatDogClassifier(**model_params)
-    else:
+    model_chose = MODEL_DISPATCH.get(model_name_clean)
+
+    if model_chose is None:
         raise ValueError(f"Model {model_name} is not supported!")
+
+    return model_chose(**model_params)
