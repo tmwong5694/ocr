de43aca022aa673cef2d1c38df340455a3d517bd
tmwong5694 <w.tinyan99@gmail.com>
2026-05-02 01:29:54 +0800

    take file as upload

--- backend/src/ocr_backend/main.py
+++ backend/src/ocr_backend/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, Request, UploadFile
 
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "success", "message": "API is online"}
+
+@app.post("/files/", tags=["files"])
+async def create_file(file: UploadFile, request: Request):
+    file_size = request.headers.get("content-length")
+    return {
+        "filename": file.filename,
+        "file_size": int(file_size) if file_size else 0,
+        "content_type": file.content_type
+    }
