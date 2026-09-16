0af13fc80f7619f90c96b559764a6340175cab29
tmwong5694 <w.tinyan99@gmail.com>
2026-09-17 01:04:21 +0800

    renamed file name (?no chg)

--- backend/backend_src/ocr_backend/main.py
+++ backend/backend_src/ocr_backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, File, Request, UploadFile
-
+from ml_pipeline.ocr_model import OCRModel
 
 app = FastAPI()
 
@@ -7,11 +7,18 @@ app = FastAPI()
 async def root():
     return {"status": "success", "message": "API is online"}
 
-@app.post("/files/", tags=["files"])
-async def create_file(file: UploadFile, request: Request):
+async def _analyze_file_stat(file: UploadFile, request: Request):
     file_size = request.headers.get("content-length")
     return {
         "filename": file.filename,
         "file_size": int(file_size) if file_size else 0,
         "content_type": file.content_type
     }
+@app.post("/analyze_file/", tags=["files"])
+async def analyze_file(file: UploadFile, request: Request):
+    return await _analyze_file_stat(file, request)
+
+@app.post("/files/ocr", tags=["files"])
+async def extract_ocr(file: UploadFile):
+
+    return
