0f747f8464e726e8a3881e90f701af30cf5686d9
tmwong5694 <w.tinyan99@gmail.com>
2026-09-17 01:42:45 +0800

    add endpoint for extract ocr

--- backend/backend_src/ocr_backend/main.py
+++ backend/backend_src/ocr_backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, Request, UploadFile
+from pathlib import Path
+from typing import Literal
 from ml_pipeline.ocr_model import OCRModel
 
 app = FastAPI()
@@ -19,6 +21,31 @@ async def analyze_file(file: UploadFile, request: Request):
     return await _analyze_file_stat(file, request)
 
 @app.post("/files/ocr", tags=["files"])
-async def extract_ocr(file: UploadFile):
+async def extract_ocr(
+        file: UploadFile,
 
+):
+    model_name = "zai-org/GLM-OCR"
+    temp_path = ""
+    prompt_path = Path("ml") / ".config" / "ocr_prompt.txt"
+
+    try:
+        with open(prompt_path, "r") as f:
+            prompt = f.read()
+    except FileNotFoundError:
+        return {"error": f"Prompt file not found at {prompt_path}"}
+
+    ocr_model = OCRModel(
+        model_name=model_name,
+        device="cpu",
+        model_kwargs={},
+        enable_preprocessing=True,
+        max_width=768,
+        max_height=768,
+    )
+    ocr_model.image_to_text(
+        image_path=temp_path,
+        prompt=prompt,
+        model_name=model_name
+    )
     return
